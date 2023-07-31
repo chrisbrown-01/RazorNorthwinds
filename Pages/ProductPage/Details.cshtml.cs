a443bc1e91ebd0423b0f6a82bb6d2633ae9ab528
chrisbrown-01 <chrisbrown1@hotmail.ca>
2023-07-31 19:56:04 -0400

    add default Product pages

---
+++ Pages/ProductPage/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorNorthwinds.Data;
+using RazorNorthwinds.Models;
+
+namespace RazorNorthwinds.Pages.ProductPage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorNorthwinds.Data.NorthwindsDbContext _context;
+
+        public DetailsModel(RazorNorthwinds.Data.NorthwindsDbContext context)
+        {
+            _context = context;
+        }
+
+      public Product Product { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/ProductPage/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorNorthwinds.Data;
+using RazorNorthwinds.Models;
+
+namespace RazorNorthwinds.Pages.ProductPage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorNorthwinds.Data.NorthwindsDbContext _context;
+
+        public IndexModel(RazorNorthwinds.Data.NorthwindsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier).ToListAsync();
+            }
+        }
+    }
+}
