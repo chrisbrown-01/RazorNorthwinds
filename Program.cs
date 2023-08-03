ec468b5d2eb3e36dcd3f7def1dd4f630b1eb2621
chrisbrown-01 <chrisbrown1@hotmail.ca>
2023-08-03 13:20:00 -0400

    prepare docker file

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ using RazorNorthwinds.Mediatr.Behaviours;
 using Serilog;
 using Serilog.Events;
 
-// TODO: README notes that this project is simply for experimenting with EF Core, Mediatr and GraphQL. Therefore no features that I would otherwise normally include such as table result pagination/sorting/filtering, overposting protections, decimal precisions in webpage renders, proper logging and exception handling, etc.
-
 namespace RazorNorthwinds
 {
     public class Program
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RazorNorthwinds
+// TODO: README notes that this project is simply for experimenting with EF Core, Mediatr and GraphQL. Therefore no features that I would otherwise normally include such as table result pagination/sorting/filtering, overposting protections, decimal precisions in webpage renders, proper logging and exception handling, etc.
+
